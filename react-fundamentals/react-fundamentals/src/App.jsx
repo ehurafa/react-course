e7e12361d149ac60fa30f5d838adb6576fb48f69
Rafael Gomes <rafarfl2010@gmail.com>
2021-03-12 20:45:29 -0300

    card component

--- react-fundamentals/react-fundamentals/src/App.jsx
+++ react-fundamentals/react-fundamentals/src/App.jsx
@@ -4,12 +4,25 @@ import First from './components/basic/First'
 import WithParameter from './components/basic/WithParameter'
 import Fragment from './components/basic/Fragment'
 import RandomNumber from './components/basic/RandomNumber'
+import Card from './components/layout/Card'
 
 export default _ =>
         <div id="app">
             <h1>Fundamentos React</h1>
-            <RandomNumber a={2} b={9}></RandomNumber>
-            <Fragment></Fragment>
-            <WithParameter titulo="Segundo componente" subtitulo="Muito legal!" nota={9.3}></WithParameter>
-            <First></First>
+            <Card titulo="Valor Aleatório">
+                <RandomNumber a={2} b={9}></RandomNumber>
+            </Card>
+
+            <Card titulo="Fragmento">
+                <Fragment></Fragment>
+            </Card>
+
+            <Card titulo="Componente com parâmetro">
+                <WithParameter titulo="Segundo componente" subtitulo="Muito legal!" nota={9.3}></WithParameter>
+            </Card>
+            
+            <Card titulo="Primeiro">
+                <First></First>
+            </Card>
+
         </div>
